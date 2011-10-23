e94410c63f64b21ed170175d68300f80c0a0485f
Michał Górny <mgorny@gentoo.org>
2011-10-23 19:11:10 +0200

    Use @OnceProperty for kernel properties.

--- ecleankernel/kernel.py
+++ ecleankernel/kernel.py
@@ -3,17 +3,49 @@
 # Released under the terms of the 2-clause BSD license.
 
 from collections import defaultdict
+from functools import partial
 from glob import glob
+
+def OnceProperty(f):
+	def _get(propname, self):
+		try:
+			return getattr(self, propname)
+		except AttributeError:
+			return None
+
+	def _set(propname, self, val):
+		if hasattr(self, propname):
+			raise KeyError('Value for %s already set' % propname)
+		return setattr(self, propname, val)
+
+	attrname = '_%s' % f.__name__
+	return property(partial(_get, attrname), partial(_set, attrname))
 
 class Kernel(object):
 	""" An object representing a single kernel version. """
 
 	def __init__(self, version):
-		self.version = version
-		self.vmlinuz = None
-		self.systemmap = None
-		self.config = None
-		self.modules = None
+		self._version = version
+
+	@property
+	def version(self):
+		return self._version
+
+	@OnceProperty
+	def vmlinuz(self):
+		pass
+
+	@OnceProperty
+	def systemmap(self):
+		pass
+
+	@OnceProperty
+	def config(self):
+		pass
+
+	@OnceProperty
+	def modules(self):
+		pass
 
 	def __repr__(self):
 		return "Kernel(%s, '%s%s%s%s')" % (repr(self.version),
