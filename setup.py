062b3b2e697639145c56ab14b5de0277e4452857
Michał Górny <mgorny@gentoo.org>
2011-11-14 22:06:23 +0100

    Fix copyright line.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #	vim:fileencoding=utf-8
-# (c) 2010 Michał Górny <mgorny@gentoo.org>
+# (c) 2011 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
 
 from distutils.core import setup
