f1e0993d359aad86cf6a66983d27a854a4b3d024
Michał Górny <mgorny@gentoo.org>
2021-08-10 22:39:39 +0200

    Bump to 2.99.3
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- ecleankernel/__init__.py
+++ ecleankernel/__init__.py
@@ -1,5 +1,5 @@
 # vim:fileencoding=utf-8
-# (c) 2011-2020 Michał Górny <mgorny@gentoo.org>
+# (c) 2011-2021 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
 
-__version__ = '2.99.2'
+__version__ = '2.99.3'
