e2eddebf2d053c4041b11ad622ebacec96c11f7a
Michał Górny <mgorny@gentoo.org>
2023-03-21 15:50:52 +0100

    Convert to flit_core
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- ecleankernel/__init__.py
+++ ecleankernel/__init__.py
@@ -1,5 +1,7 @@
 # vim:fileencoding=utf-8
-# (c) 2011-2021 Michał Górny <mgorny@gentoo.org>
+# (c) 2011-2023 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
+
+"""Installed kernel cleanup tool"""
 
 __version__ = '2.99.3'
