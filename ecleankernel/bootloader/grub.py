6504feb66fca84b44c8905cc2f83e3e348c28cba
Andrew Ammerlaan <andrewammerlaan@gentoo.org>
2024-10-26 14:41:39 +0200

    bootloader: consider GRUB_CFG as location for grub config
    sys-kernel/installkernel[grub] supports setting GRUB_CFG in the environment to
    update a grub.cfg in a non-standard location. Specifically this is useful with
    sys-boot/grub[secureboot] where the built grub efi executable loads the
    grub.cfg from the same directory it is in.
    
    Recognize the same environment variable here so we don't accidentally update
    the wrong config.
    
    Signed-off-by: Andrew Ammerlaan <andrewammerlaan@gentoo.org>

--- ecleankernel/bootloader/grub.py
+++ ecleankernel/bootloader/grub.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import logging
+import os
 import os.path
 import typing
 
@@ -11,7 +12,10 @@ from ecleankernel.bootloader.lilo import LILO
 class GRUB(LILO):
     name = 'grub'
     kernel_re = r'^\s*(kernel|module)\s*(\([^)]+\))?(?P<path>\S+)'
-    def_path = ('/boot/grub/menu.lst', '/boot/grub/grub.conf')
+    def_path = (os.environ.get("GRUB_CFG"),
+                '/boot/grub/menu.lst',
+                '/boot/grub/grub.conf',
+                )
 
     def _get_kernels(self,
                      content: str
